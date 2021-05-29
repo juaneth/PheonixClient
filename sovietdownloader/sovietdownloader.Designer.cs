231605603eba674dc151eaf92e4062f1f6f9872d
Juan <euanwalker08@outlook.com>
2021-05-29 23:11:47 +0100

    Add project files.

---
+++ sovietdownloader/sovietdownloader.Designer.cs
@@ -0,0 +1,55 @@
+﻿
+namespace sovietdownloader
+{
+    partial class sovietdownloaderpage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(sovietdownloaderpage));
+            this.SuspendLayout();
+            // 
+            // sovietdownloaderpage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::sovietdownloader.Properties.Resources.ddsng7u_0fc8f5fa_af5f_4e7a_b4a4_9a1be6be5d29;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1022, 586);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "sovietdownloaderpage";
+            this.ShowIcon = false;
+            this.Text = "SovietDownloader";
+            this.Load += new System.EventHandler(this.sovietdownloaderpage_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
