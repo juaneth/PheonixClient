e11bf6a662f6dc23b7d4465e43fda3aaafb94048
Juan <euanwalker08@outlook.com>
2021-06-01 21:06:29 +0100

    yeah i dont even know what i did

--- CommieClient/cmclient.Designer.cs
+++ CommieClient/cmclient.Designer.cs
@@ -30,24 +30,37 @@ namespace CommieClient
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Location = new System.Drawing.Point(-4, -6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1161, 692);
+            this.panel1.Size = new System.Drawing.Size(1161, 693);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // button1
             // 
-            this.panel2.Location = new System.Drawing.Point(44, 30);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1056, 220);
-            this.panel2.TabIndex = 0;
+            this.button1.Location = new System.Drawing.Point(70, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(338, 141);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "store";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(728, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(338, 141);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "library";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // cmclient
             // 
@@ -66,6 +79,7 @@ namespace CommieClient
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
