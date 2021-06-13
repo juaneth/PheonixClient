2c7cfc126df81723c6aa740383968a25db41544f
Juan <euanwalker08@outlook.com>
2021-06-13 20:29:42 +0100

    made simple UI

--- CommieClient/cmclient.Designer.cs
+++ CommieClient/cmclient.Designer.cs
@@ -30,48 +30,28 @@ namespace CommieClient
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(-4, -6);
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(12, 155);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1161, 693);
+            this.panel1.Size = new System.Drawing.Size(848, 465);
             this.panel1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(70, 220);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(338, 141);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "store";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(728, 220);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(338, 141);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "library";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // cmclient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1150, 679);
+            this.ClientSize = new System.Drawing.Size(872, 632);
             this.Controls.Add(this.panel1);
             this.Name = "cmclient";
             this.Text = "main";
             this.Load += new System.EventHandler(this.cmclient_Load);
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -79,7 +59,5 @@ namespace CommieClient
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
     }
 }
