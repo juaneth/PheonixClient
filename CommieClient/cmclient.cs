b0522c24a817e8e5140f69d58c9535b0ddbf3c4e
Juan <euanwalker08@outlook.com>
2021-06-29 00:26:43 +0100

    chnaging whole repo system to using modified zips

--- CommieClient/cmclient.cs
+++ CommieClient/cmclient.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-using System.Net;
 using System.IO;
 using System.Linq;
 
@@ -15,72 +14,14 @@ namespace CommieClient
 
         private void cmclient_Load(object sender, EventArgs e)
         {
+            //App Startup Path
+            string appPath = Application.StartupPath;
             //grab where the fuck the repo is
             string repolocation = File.ReadLines("savedrepo.comclient").Skip(0).Take(1).First();
 
-            //Load UI
-
-            //This is all pretty messy so i should make a way to do this without repeating this 60 times
-
-            var repolength = File.ReadAllLines(repolocation).Length.ToString();
-            MessageBox.Show(repolength);
-            var repolengthint = File.ReadAllLines(repolocation).Length;
-            int sum = repolengthint + 1;
-            int newsum = sum / 7;
-            string newsumstring = newsum.ToString();
-            MessageBox.Show(newsumstring);
-
-
-
-
-
-            //First Panel's shite
-            string itemname = File.ReadLines(repolocation).Skip(0).Take(1).First();
-            name1.Text = itemname;
-            string itemid = "Item ID: " + File.ReadLines(repolocation).Skip(3).Take(1).First();
-            idtext1.Text = itemid;
-
-            //Second Panel's shite
-            string itemname2 = File.ReadLines(repolocation).Skip(7).Take(1).First();
-            name2.Text = itemname2;
-            string itemid2 = "Item ID: " + File.ReadLines(repolocation).Skip(10).Take(1).First();
-            idtext2.Text = itemid2;
-
-            //Third Panel's shite
-            string itemname3 = File.ReadLines(repolocation).Skip(14).Take(1).First();
-            name3.Text = itemname3;
-            string itemid3 = "Item ID: " + File.ReadLines(repolocation).Skip(17).Take(1).First();
-            idtext3.Text = itemid3;
-
-            //Fourth Panel's shite
-            string itemname4 = File.ReadLines(repolocation).Skip(21).Take(1).First();
-            name4.Text = itemname4;
-            string itemid4 = "Item ID: " + File.ReadLines(repolocation).Skip(24).Take(1).First();
-            idtext4.Text = itemid4;
-
-            //Fifth Panel's shite
-            string itemname5 = File.ReadLines(repolocation).Skip(28).Take(1).First();
-            name5.Text = itemname5;
-            string itemid5 = "Item ID: " + File.ReadLines(repolocation).Skip(31).Take(1).First();
-            idtext5.Text = itemid5;
-
-            //Sixth Panel's shite
-            string itemname6 = File.ReadLines(repolocation).Skip(35).Take(1).First();
-            name6.Text = itemname6;
-            string itemid6 = "Item ID: " + File.ReadLines(repolocation).Skip(38).Take(1).First();
-            idtext6.Text = itemid6;
-
-            //Seventh Panel's shite
-            string itemname7 = File.ReadLines(repolocation).Skip(42).Take(1).First();
-            name7.Text = itemname7;
-            string itemid7 = "Item ID: " + File.ReadLines(repolocation).Skip(45).Take(1).First();
-            idtext7.Text = itemid7;
-
-            //Eighth Panel's shite
-            string itemname8 = File.ReadLines(repolocation).Skip(49).Take(1).First();
-            name8.Text = itemname8;
-            string itemid8 = "Item ID: " + File.ReadLines(repolocation).Skip(52).Take(1).First();
-            idtext8.Text = itemid8;
+            //Unpack repo file
+            //rename
+            File.Move(repolocation, appPath + "repofile.zip");
         }
     }
 }
